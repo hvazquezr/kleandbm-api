f5ecf873c309b3719fe98d2709b5c8efe98bd471
Hector Vazquez <hector.vazquez@jaesis.com>
2024-01-12 01:03:09 -0500

    Fixed issued with aiohttp and openai. Downgraded to Python 3.11

--- services/project_service.py
+++ services/project_service.py
@@ -11,21 +11,22 @@ class ProjectService:
     producer = Producer({'bootstrap.servers':settings.kafka_server})
 
     @staticmethod
-    def kafka_produce(topic, key, value, use_local_producer=False):
+    def kafka_produce(topic, key, value):
         # Due to this celeri/kafka-python issue the producer has to be local when called from a celeri worker
         # https://github.com/dpkp/kafka-python/issues/1098
-        if use_local_producer:
-            producer = Producer({'bootstrap.servers':ProjectService.settings.kafka_server})
-            producer.produce(topic, key=key, value=value)
-            producer.flush()
-        else:
-            ProjectService.producer.produce(topic, key=key, value=value)
-            ProjectService.producer.flush()
+        producer = Producer({'bootstrap.servers':ProjectService.settings.kafka_server})
+        producer.produce(topic, key=key, value=value)
+        producer.flush()
+
+    @staticmethod
+    async def async_kafka_produce(topic, key, value):
+        ProjectService.producer.produce(topic, key=key, value=value)
+        ProjectService.producer.flush()
 
     # TODO: Change the return type to Project and handle that in Celeri
     @staticmethod
     def create_project(new_project: ProjectCreate):
-        ProjectService.kafka_produce('project-updates', new_project.id, new_project.model_dump_json(), use_local_producer=True)
+        ProjectService.kafka_produce('project-updates', new_project.id, new_project.model_dump_json())
         return ({'result': 'success'})    
     
     @staticmethod
--- services/utils.py
+++ services/utils.py
@@ -1,7 +1,9 @@
 import json
 import re
 import aiohttp
+from openai import OpenAI
 
+# To be used for requests to ksql but it's global for performance reasons
 session = aiohttp.ClientSession()
 
 async def query_ksql(kdsqldb_cluster, ksql_query):
@@ -16,7 +18,7 @@ async def query_ksql(kdsqldb_cluster, ksql_query):
 
     try:
         # Use the provided session to make the HTTP request
-        async with session.post(kdsqldb_cluster + '/query', headers=headers, data=data) as response:
+        async with session.post(kdsqldb_cluster + '/query', headers=headers, data=data, ssl=False) as response:
             if response.status == 200:
                 return await response.json()
             else:
@@ -26,7 +28,7 @@ async def query_ksql(kdsqldb_cluster, ksql_query):
         print(f"Error querying ksqlDB: {e}")
         return []
 
-    
+# Formats replies from ksql in a more intuitive json format   
 def process_ksql_response(json_data):
     # Extract the schema from the first element
     schema_str = re.sub(r'<.*?>', '', json_data[0]['header']['schema'])
@@ -41,3 +43,51 @@ def process_ksql_response(json_data):
         row_dict = dict(zip(column_names, row_data))
         result.append(row_dict)
     return result
+
+# Retrieve only the text between the markers ```json and ``` from OpenAI API responses
+def extract_json_text(input_text):
+    # Define a regular expression pattern to match text between ```json and ```
+    pattern = r'```json(.*?)```'
+    
+    # Use re.DOTALL to match across multiple lines
+    match = re.search(pattern, input_text, re.DOTALL)
+    
+    if match:
+        # Extract and return the matched text
+        json_text = match.group(1).strip()
+        return json_text
+    else:
+        return None
+
+# Prompts OpenAI
+def prompt_openai(openai_key, model, system_message, user_message):
+    client = OpenAI(api_key=openai_key)
+
+    try:
+      response = client.chat.completions.create(
+        model=model,
+        messages=[
+          {
+            "role": "system",
+            "content": system_message
+          },
+          {
+            "role": "user",
+            "content": user_message
+          }
+        ],
+        temperature=0,
+        max_tokens=2900,
+        #top_p=0,
+        frequency_penalty=0,
+        presence_penalty=0,
+        seed=0,
+        #response_format={ "type": "json_object" }
+      )
+      print(system_message)
+      print(user_message)
+      print(response.choices[0].message.content)
+      return extract_json_text(response.choices[0].message.content)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
